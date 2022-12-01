de6ba19bf6ad7433be6d09c8c5d3519a399baa3a
Marcelo Antonio <inho.menezes2011@gmail.com>
2022-11-30 21:14:32 -0300

    Adding functions: Insert, Maximum, Minimum, Successor and Predecessor

--- binaryTree.py
+++ binaryTree.py
@@ -2,7 +2,7 @@
 class Node:
 
     # Constructor method that creates the node root
-    def __init__(self, data):
+    def __init__(self, data: int):
         self.data = data
         self.leftSon = None
         self.rightSon = None
@@ -73,11 +73,14 @@ class Node:
 class BinaryTree:
 
     # Creates a binary tree (by default, a binary tree is created empty)
-    def __init__(self, node: Node):
+    def __init__(self, node = None):
         self.root = node
 
+    def getRoot(self):
+        return self.root
+
     # Function that recursively prints the whole tree (Preorder)
-    def preOrderTransverse(self, pointer: Node):
+    def preOrderTransverse(self, pointer):
 
         # Verifies if the tree is empty
         if self.root == None:
@@ -94,24 +97,123 @@ class BinaryTree:
             # Prints the data of the right son
             self.preOrderTransverse(pointer.getRightSon())
 
+    # Function that searches for a given node in the tree
+    def search(self, pointer:Node, node: Node):
+        # Base case, to stop the recursive function
+        if pointer.getData() == None or node.getData() == pointer.getData():
+            return pointer
+
+        # Verifies if the value to be found is lower than the pointer, then if it is, call the left son
+        if node.getData() < pointer.getData():
+            return self.search(pointer.getLeftSon(), node)
+
+        # If not, call the right son instead
+        else:
+            return self.search(pointer.getRightSon(), node)
+
+    # Returns the more to the left node of the tree
+    def minimum(self, pointer = None):
+        # Verifies if a starting point was given to start the search
+        if pointer == None:
+            # If there's not a given point, the search will begin with the root
+            pointer = self.getRoot()
+
+        # Will repeat until the node more to the left is found
+        while pointer.getLeftSon() != None:
+            # If it's not found yet, go to the left son
+            pointer = pointer.getLeftSon()
+        return pointer
+
+    # Returns the more to the right node of the tree
+    def maximum(self, pointer = None):
+        # Verifies if a starting point was given to start the search
+        if pointer == None:
+            # If there's not a given point, the search will begin with the root
+            pointer = self.getRoot()
+
+        # Will repeat until the node more to the right is found
+        while pointer.getRightSon() != None:
+            # If it's not found yet, go to the right son
+            pointer = pointer.getRightSon()
+        return pointer
+
+    # Returns the node with the lowest value but bigger than the value of the given node
+    def successor(self, node: Node):
+
+        # Verifies is there is a right sub-tree (because the successor is the minimum of that sub-tree)
+        if node.getRightSon() != None:
+            return self.minimum(node.getRightSon())
+        
+        pointer = node.getFather()
+
+        # Search for the first ancestral whose left son is also a father of the given node
+        while pointer != None and node == pointer.getRightSon():
+            # Swapping the values of the variable
+            node, pointer = pointer, pointer.getFather()
+        
+        # Return the successor
+        return pointer
+
+     # Returns the node with the biggest value but lower than the value of the given node
+    def predecessor(self, node: Node):
+
+        # Verifies is there is a left sub-tree (because the successor is the maximum of that sub-tree)
+        if node.getLeftSon() != None:
+            return self.maximum(node.getLeftSon())
+        
+        pointer = node.getFather()
+
+        # Search for the first ancestral whose right son is also a father of the given node
+        while pointer != None and node == pointer.getLeftSon():
+            # Swapping the values of the variable
+            node, pointer = pointer, pointer.getFather()
+        
+        # Return the successor
+        return pointer
+    
+    # Function that inserts a new node into the binary tree  
+    def insert(self, node: Node):
+        # Starting point of iteration
+        pointer = self.getRoot()
+        temp = None
+
+        # Verifies whether the value is lower or bigger than the pointer to insert
+        while pointer != None:
+            # Store the value of the current node
+            temp = pointer
+            # Verifies where to place the new node (left or right sub-tree)
+            if node.getData() < pointer.getData():
+                pointer = pointer.getLeftSon()
+            else: 
+                pointer = pointer.getRightSon()
+
+        # Temp becomes the given node father
+        node.father = temp
+
+        # Verifies if the tree is empty, if it is, then the given value becomes the root
+        if temp == None:
+            self.root = node
+
+        # Verifies if the given node is lower than the current node
+        elif node.getData() < temp.getData():
+            temp.leftSon = node
+        # The given node is bigger than the current node
+        else:
+            temp.rightSon = node
+
 # Main program
+tree = BinaryTree()
 
-node1 = Node(40)
-
-# Creates a binary tree and puts 'node1' as it's root node
-tree = BinaryTree(node1)
-
-# Creates two more nodes as 'node1' left and right sons
-node1.leftSon = Node(20)
-node1.rightSon = Node(60)
-
-node1.getLeftSon().leftSon = Node(50)
-node1.getRightSon().rightSon = Node(70)
-
-node1.getLeftSon().leftSon = Node(10)
-node1.getLeftSon().rightSon = Node(30)
-
-# print(node1.getData())
-# print('Filho esquerdo:', node1.getLeftSon().getData())
-# print('Filho direito:', node1.getRightSon().getData())
-tree.preOrderTransverse(node1)
+tree.insert(Node(25))
+tree.insert(Node(20))
+tree.insert(Node(36))
+tree.insert(Node(10))
+tree.insert(Node(22))
+tree.insert(Node(5))
+tree.insert(Node(12))
+node1 =  Node(30)
+tree.insert(node1)
+tree.insert(Node(40))
+tree.insert(Node(28))
+tree.insert(Node(38))
+tree.insert(Node(48))
